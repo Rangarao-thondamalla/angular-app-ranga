cd23058869511ceb90eee53018512b9da3b6bad4
Rangarao-thondamalla <t.rangarao007@gmail.com>
2019-11-06 01:22:32 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ranga time is: ';
+  name = 'Angular ranga time is ranga: ';
   url = 'https://www.youtube.com/watch?v=k5E2AVpwsko';
 }
