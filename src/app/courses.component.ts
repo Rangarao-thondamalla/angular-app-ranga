12cd98349abad7456a665e42883bfc31a6330416
Rangarao-thondamalla <t.rangarao007@gmail.com>
2019-11-13 13:20:38 +0530

    4 files modified

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -9,6 +9,14 @@ import { CoursesService} from './courses.service';
           {{course}}
         </li>
       </ul>
+      <button (click)="onSave()">Save</button><br>
+      <input [(ngModel)]="email"(keyup.enter)="onKeyUp()"/><br>
+      {{courseData.title | uppercase}}<br>
+       {{courseData.rating}}<br>
+        {{courseData.price}}<br>
+         {{courseData.students}}<br>
+          {{courseData.releaseData}}<br>
+
   `
 })
 export class CoursesComponent{
@@ -19,5 +27,19 @@ export class CoursesComponent{
   constructor (service: CoursesService){
     this.courses = service.getCourses();
   }
+   onSave(){
+     console.log("button clicked");
+   } 
 
+  email ="me@email.com";
+  onKeyUp(email){
+        console.log(this.email);
+  }
+  courseData = {
+    title: "angular app",
+    rating: 3.459,
+    students : 30520,
+    price :190.92,
+    releaseData: new Date(2019, 11 , 12)
+  }
 }
