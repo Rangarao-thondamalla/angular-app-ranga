cdb48aeed34ac911844154fb4b944a2fe279f72c
Rangarao-thondamalla <t.rangarao007@gmail.com>
2019-12-03 01:05:14 +0530

    3 files modified

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -8,11 +8,37 @@ import { Http } from "@angular/http";
 })
 export class PostsComponent {
   posts: any[];
+  private url = 'https://jsonplaceholder.typicode.com/posts';
 
-  constructor(http: Http){
-    http.get('https://jsonplaceholder.typicode.com/posts')
+  constructor(private http: Http){
+    http.get(this.url)
     .subscribe(response =>{
       this.posts = response.json();
     })
+  }
+  createPost(input : HTMLInputElement){
+    let post = {title : input.value};
+   // event.value = '';
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+           post['id'] = response.json().id;
+           this.posts.push;
+    });
+  }
+  updatePost(post){
+    this.http.patch(this.url + '/' + post.id  , JSON.stringify(post))
+    .subscribe(response=>{
+      console.log(response.json());
+    })
+    //this.http.put(this.url , JSON.stringify(post));
+  }
+  deletePost(post){
+    this.http.delete(this.url + '/' + post.id)
+    .subscribe(response=>{
+      let index = this.posts.indexOf(post);
+      this.posts.splice(index,1);
+      //console.log(response.json());
+    })
+    //this.http.put(this.url , JSON.stringify(post));
   }
 }
